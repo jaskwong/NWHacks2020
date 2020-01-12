f76fdbab3ee102351fb1f43bc6c86944d05fb0a1
Jasmine Kwong <jasminekkwong@gmail.com>
2020-01-12 12:01:22 -0800

    map updating

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var mysql = require('mysql');
+var bodyParser = require('body-parser')
+var app = express()
 
 var con = mysql.createConnection({
     host: 'localhost',
@@ -9,34 +11,65 @@ var con = mysql.createConnection({
     database: 'nwhacks2020'
 });
 
-router.get("/new",(req,res)=>{
-	res.render("newEvents");
-});
-
-router.post("/new",(req,res)=>{
-    //create new events and send that shit to MySQL 
-	res.send("You reached the post event route");
-});
-
-router.get(":id",(req,res)=>{
-    res.send("You reached the more info page");
-})
-
-router.get(":id/edit", (req,res)=>{
-    res.send("you reached the edit events page");
-})
-
-router.put(":id", (req,res)=>{
-    res.send("You are trying to update this event");
-})
-
-router.delete(":id", (req,res)=>{
-    res.send("You are trying to delete this event");
-})
 
 var events;
 var orgs;
 var result;
+
+    
+router.get("/new", (req, res) => {
+    res.render("newEvents");
+});
+
+
+
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json())
+
+router.post("/new", (req, res) => {
+    var s = "INSERT INTO events VALUES (" + (Math.floor(Math.random()*90000)+10000) + ", '" + req.body.Event_Address + "', TIMESTAMP ('" + req.body.Event_Time + "'), " + req.body.Event_Ppl + ", '" + req.body.Company_Usnm + "')"  
+    con.query(s, function(err, res) {
+        if (err) throw err;
+        con.query("select * from events", function (err, res) {
+            console.log(res)
+        })
+    })
+    con.connect(function (err) {
+        var s = "SELECT * FROM events"
+        con.query(s, function (err, res) {
+            events = JSON.stringify(res)
+            var s2 = "SELECT organization.o_name FROM events, organization WHERE events.o_usnm = organization.o_usnm"
+            con.query(s2, function (err2, res2) {
+                orgs = JSON.stringify(res2)
+                result = events + " SEP " + orgs
+                console.log(result)
+                router.get('/', function (req, res) {
+                    res.render('events', {
+                        res: JSON.stringify(result)
+                    })
+                });
+            })
+        })
+    })
+    res.redirect("/events");
+});
+
+router.get(":id", (req, res) => {
+    res.send("You reached the more info page");
+})
+
+router.get(":id/edit", (req, res) => {
+    res.send("you reached the edit events page");
+})
+
+router.put(":id", (req, res) => {
+    res.send("You are trying to update this event");
+})
+
+router.delete(":id", (req, res) => {
+    res.send("You are trying to delete this event");
+})
+
 
 con.connect(function (err) {
     var s = "SELECT * FROM events"
@@ -55,5 +88,6 @@ con.connect(function (err) {
         })
     })
 })
+
 
 module.exports = router;
